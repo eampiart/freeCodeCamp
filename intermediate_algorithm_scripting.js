5b726d678c711a388f63da078220969eb90dfebd
Petra Donka <donkapetra@gmail.com>
2016-03-21 14:19:54 +0100

    Added add - last challenge in Intermediate Algorithm Scripting

--- intermediate_algorithm_scripting.js
+++ intermediate_algorithm_scripting.js
@@ -309,3 +309,22 @@ function every(collection, pre) {
 
 // Testing every
 // console.log(every([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
+
+// Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+// For example, add(2, 3) should return 5, and add(2) should return a function.
+// Calling this returned function with a single argument will then return the sum:
+// var sumTwoAnd = add(2);
+// sumTwoAnd(3) returns 5.
+// If either argument isn't a valid number, return undefined.
+function add(arg1, arg2) {
+    if (typeof(arg1) !== 'number' || (arg2 !== undefined && typeof(arg2) !== 'number')) {
+        return undefined;
+    } else if (arg2 === undefined) {
+        return function(n){ return add(arg1,n); };
+    } else {
+        return arg1 + arg2;
+    }
+}
+
+// Testing add
+// console.log(add(2,"3"));
