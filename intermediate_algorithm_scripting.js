db544d1ff812b2c34499f8bba5fca4000b8cdc33
Petra Donka <donkapetra@gmail.com>
2016-03-21 13:52:13 +0100

    Added every

--- intermediate_algorithm_scripting.js
+++ intermediate_algorithm_scripting.js
@@ -295,3 +295,17 @@ function binaryAgent(str) {
 
 // Testing binaryAgent
 // console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
+
+// Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+function every(collection, pre) {
+    // Is everyone being true?
+    return collection.reduce(function(truthy,c){
+        if (!c[pre]) {
+            return false;
+        }
+        return truthy;
+    },true);
+}
+
+// Testing every
+// console.log(every([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
